226ca5033b37d1e5c3d199eb41dc45230ce75d19
Kelton Benson <kelton01@csu.fullerton.edu>
2023-10-27 23:05:12 -0700

    works on new node verison

--- app.js
+++ app.js
@@ -1,50 +1,111 @@
-require('dotenv').config();
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const User = require('./database/User')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require("./database/User");
+const bcrypt = require('bcrypt');
 
-//const mysql = require("mysql2/promise");
-const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 
-const connectDB = require('./database/db')
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect("mongodb+srv://fearlocity:fear@card.fzquwhr.mongodb.net/card_info?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-connectDB();
+// Create a Mongoose model for your data
+const UserInfo = mongoose.model('UserInfo', {
+  backgroundColor: String,
+  fontFamily: String,
+  name: String,
+  position: String,
+  number: String,
+  email: String,
+  website: String,
+});
 
-
-
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/save-data", async (req, res) => {
-  try {
-    const data = req.body;
-    if (data) {
-      const [results, fields] = await connection.execute(
-        "INSERT INTO user_info (background_color, font_family, name, position, phone_number, email, website) VALUES (?, ?, ?, ?, ?, ?, ?)",
-        [
-          data.backgroundColor,
-          data.fontFamily,
-          data.name,
-          data.position,
-          data.number,
-          data.email,
-          data.website,
-        ]
-      );
-      console.log("Data saved successfully:", results);
-      res.status(200).json({ message: "Data saved successfully." });
-    } else {
-      res.status(400).json({ message: "Invalid data sent." });
-    }
-  } catch (error) {
-    console.error("Error saving data to the database:", error);
-    res.status(500).json({ message: "Failed to save data." });
+app.post('/save-data', (req, res) => {
+  const data = req.body;
+
+  if (data) {
+    // Create a new UserInfo document
+    const userInfo = new UserInfo(data);
+
+    // Save it to the MongoDB database
+    userInfo
+      .save()
+      .then(() => {
+        console.log('Data saved successfully');
+        res.status(200).json({ message: 'Data saved successfully.' });
+      })
+      .catch((error) => {
+        console.error('Error saving data:', error);
+        res.status(500).json({ message: 'Failed to save data.' });
+      });
+  } else {
+    res.status(400).json({ message: 'Invalid data sent.' });
   }
-})
+});
+
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already in use." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    // res.status(201).json({ message: "Registration successful." });
+    res.status(201).sendFile(path.join(__dirname, '/public/index.html'))
+  } catch (error) {
+    console.error("Registration error:", error);
+    // res.status(500).json({ message: "Registration failed." });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid username or password." });
+    }
+
+     // res.status(200).json({ message: "Login successful." });
+     res.status(200).sendFile(path.join(__dirname, '/public/app.html'));
+  } catch (error) {
+    console.error("Login error:", error);
+    // res.status(500).json({ message: "Login failed." });
+  }
+});
 
 const server = http.createServer(app);
 
@@ -52,52 +113,4 @@ const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
-
-function insertPostData() {
-  User.insertMany([
-
-  ])
-}
-
-
-app.get('/home', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/home.html'));
-
-
 });
-
-app.post('register', async (req, res) => {
-    
-  // const data = new LogInCollection({
-  //     name: req.body.name,
-  //     password: req.body.password
-  // })
-  // await data.save()
-
-  const data = {
-      user: req.body.email,
-      password: req.body.password
-  }
-
-  const checking = await LogInCollection.findOne({ email: req.body.email })
-
- try{
-  if (checking.email === req.body.email && checking.password===req.body.password) {
-      res.send("user details already exists")
-  }
-  else{
-      await LogInCollection.insertMany([data])
-  }
- }
- catch{
-  res.send("wrong inputs")
- }
-
-  res.status(201).sendFile(path.join(__dirname, '/public/home.html'), {
-      naming: req.body.email
-  })
-})
-
-
-  
--- database/User.js
+++ database/User.js
@@ -1,45 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-    Username: {
-        type: String,
-        required: true
-    },
-    Password: {
-        type: String,
-        required: true
-    },
-    Name: {
-        type: String,
-        default: 'Name Here'
-    },
-    Position: {
-        type: String,
-        default: 'Position Here'
-    },
-    Phone: {
-        type: String,
-        default: 'Phone: 123-456-7890'
-    },
-    Email: {
-        type: String,
-        default: 'Email: name@example.com'
-    },
-    Website: {
-        type: String,
-        default: 'Website: www.example.com'
-    },
-    Background: {
-        type: String,
-        default: '#E8E8E8'
-    },
-    Font: {
-        type: String,
-        default: 'Arial, sans-serif'
-    }
+const userSchema = new mongoose.Schema({
+  username: {
+      type: String,
+      required: true
+  },
+  password: {
+      type: String,
+      required: true
+  },
+  name: {
+      type: String,
+      default: 'Name Here'
+  },
+  position: {
+      type: String,
+      default: 'Name Here'
+  },
+  phone: {
+      type: String,
+      default: 'Phone: 123-456-7890'
+  },
+  email: {
+      type: String,
+      default: 'Email: name@example.com'
+  },
+  website: {
+      type: String,
+      default: 'www.example.com'
+  },
+  backgroundColor: {
+      type: String,
+      default: '#E8E8E8'
+  },
+  font: {
+      type: String,
+      default: 'Arial, sans-serif'
+  }
 });
 
-const collection = new mongoose.model("UserCollection",UserSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = collection
+module.exports = User;
